ba74cd7855ed88220958b2a369efc8ab9f6a3e55
usama-mahmood-tc <usamamahmood059@gmail.com>
2025-08-28 15:33:08 +0500

    added authorization

--- app/core/security.py
+++ app/core/security.py
@@ -8,7 +8,7 @@ def create_access_token(data: dict, expire_time=15):
     to_encode.update({'exp': expire})
     return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
     
-def verify_access_token(token: str):
+def verify_token(token: str):
     try:
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=settings.JWT_ALGORITHM)
         return payload
